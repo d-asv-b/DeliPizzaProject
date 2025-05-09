bc322b264eacd3588f3cf62e2e5374a39184c0d7
d-asv-b <dlasvlb@gmail.com>
2025-05-09 21:12:34 +0300

    Cerates profile info endpoint
    - Adds an API endpoint to retrieve user profile information.
    - Creates decorator that verifies access token for protected routes.
    - Refactoring in Views.

---
+++ backend/api/decorators.py
@@ -0,0 +1,44 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework_simplejwt.tokens import AccessToken
+
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+def access_token_required(view):
+    def wrapper(request: Request, *args, **kwargs):
+        auth_header = request._request.headers.get("Authorization", "")
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return Response(
+                {
+                    "error": "Invalid or missing access token!",
+                },
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        access_token = auth_header.split(" ")[1]
+
+        try:
+            token = AccessToken(access_token)
+            user_id = token.get("user_id")
+            user_obj = User.objects.get(id=user_id)
+            print(user_obj)
+        except Exception as err:
+            print(err)
+            return Response(
+                {
+                    "error": "Invalid or missing access token!",
+                },
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        request.user = user_obj
+
+        return view(request, *args, **kwargs)
+
+    return wrapper
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import userSignUpView, userSignInView, PizzaListViewSet
+from .views import user_sign_up_view, user_sign_in_view, \
+    get_profile_info, refresh_access_token, PizzaListViewSet
 
 urlpatterns = [
     path("pizzas/get_list/", PizzaListViewSet.as_view()),
 
-    path("account/signUp", userSignUpView),
-    path("account/signIn", userSignInView),
+    path("account/sign_up", user_sign_up_view),
+    path("account/sign_in", user_sign_in_view),
+    path("account/refresh_token", refresh_access_token),
+
+    path("account/profile", get_profile_info),
 ]
